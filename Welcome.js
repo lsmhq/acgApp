b2649c073759e251efa2cc7cd57441f9f56ba88f
daifang <784433957@qq.com>
2020-05-22 17:07:53 +0800

    goods

--- Welcome.js
+++ Welcome.js
@@ -11,9 +11,9 @@ export default class WelcomePage extends Component {
                     showsHorizontalScrollIndicator={false}
                 >
                     <View style={{width:450,height:200}}><Image width='100%'  source={require('./image/screen1.jpg')}/></View>
-                    <View style={{width:450,height:200}}><Image width='100%'  source={require('./image/screen1.jpg')}/></View>
+                    <View style={{width:450,height:200}}><Image width='100%'  source={require('./image/screen2.jpg')}/></View>
                     <View style={{width:450,height:200}}>
-                        <Image width='100%'  source={require('./image/screen1.jpg')}/>
+                        <Image width='100%'  source={require('./image/screen3.jpg')}/>
                         <View 
                             style = {{position:'absolute',right:50,top:50,backgroundColor:'#CAE1FF',width:150,height:50,borderRadius:10}}
                             onTouchEnd = {()=>{
